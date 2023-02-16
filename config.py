b7206d6d55f98c62be2ee9b4e5985fe347d5b3c9
moizgillani <38466692+moizgillani@users.noreply.github.com>
2023-02-16 19:50:26 +0500

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-NOTION_API_KEY = 'secret_dE9hl248cv9GNl4EcmJ6bDSM7ZJCqf2zI8bH9PmwgTH'
-NOTION_DATABASE_ID = 'ad9df9682f7a441d98669ea9dc860e91'
-
-
-SCOPES = ["https://www.googleapis.com/auth/tasks"]
